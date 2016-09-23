4e93ff05a49ef61e3dd28bf0603d5e9850ad5db5
Andrew Chumich <andrewchumich@gmail.com>
2016-09-23 15:17:10 -0600

    initial commit

--- src/common/hero/heroList/heroList.js
+++ src/common/hero/heroList/heroList.js
@@ -16,5 +16,25 @@ function heroList() {
 }
 
 function heroListController($scope) {
-    
+    $scope.selectedSide = null;
+
+    $scope.options = [
+        {
+            label: 'Light',
+            value: true
+        },
+        {
+            label: 'Dark',
+            value:  false
+        }
+    ];
+
+    $scope.heroFilter = function(hero) {
+        if ($scope.selectedSide === null) {
+            // if selectedSide is null, we should show all values
+            return true;
+        } else {
+            return hero.light === $scope.selectedSide.value;
+        }
+    };
 }
